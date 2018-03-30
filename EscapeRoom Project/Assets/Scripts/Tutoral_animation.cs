f853f262331fa35735fb4bd36a3cdfa021681323
markfvn <markvandernaald@gmail.com>
2018-03-29 20:36:10 -0500

    It lives again

---
+++ EscapeRoom Project/Assets/Scripts/Tutoral_animation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Tutoral_animation : MonoBehaviour
+    {
+    public BoxCollider keypad;
+        Player player;
+        Animator anim;
+    public AudioClip Sound;
+    public AudioSource playsound;
+        private void Awake()
+        {
+            player = GameObject.Find("Player").GetComponent<Player>();
+            anim = GetComponent<Animator>();
+            playsound.clip = Sound;
+        }
+
+
+
+        private void FixedUpdate()
+        {
+            bool test = player.rightController.GetPressDown(SteamVR_Controller.ButtonMask.Grip);
+            
+            anim.SetBool("Open", test);
+
+        bool test2 = player.rightController.GetPressDown(SteamVR_Controller.ButtonMask.Trigger);
+        if (test2)
+        {
+            playsound.Play();
+            playsound.clip = Sound;
+        }
+        }
+
+    }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# VR_EscapeRoom
+# Escape Room VR Project
