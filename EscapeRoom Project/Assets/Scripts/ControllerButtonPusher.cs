2ba87efdb9248a6beae67b6b047c8922a94f4d8f
39xdgy <wangjiashu3@126.com>
2018-03-30 16:44:19 -0500

    Keypad connect to the door
    key is 1111, use the controller would open the door

--- EscapeRoom Project/Assets/Scripts/ControllerButtonPusher.cs
+++ EscapeRoom Project/Assets/Scripts/ControllerButtonPusher.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class ControllerButtonPusher : MonoBehaviour {
 
-    private ArrayList code;
-	private ArrayList rightCode;
+    private String code;
+	private String rightCode;
 	public AudioSource hit;
 	public AudioSource wrong;
 	public AudioSource right;
@@ -18,19 +19,15 @@ public class ControllerButtonPusher : MonoBehaviour {
 
     private void setText(int num)
     {
-        CodeNum.text += num + " ";
+        CodeNum.text += num.ToString() + " ";
     }
 
 
 
 	void Start ()
 	{
-        rightCode = new ArrayList();
-        rightCode.Add(1);
-        rightCode.Add(2);
-        rightCode.Add(3);
-        rightCode.Add(4);
-        code = new ArrayList();
+        rightCode = "1111";
+        code = "";
         CodeNum.text = "Code: ";
     }
 
@@ -43,96 +40,83 @@ public class ControllerButtonPusher : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("Zero"))
         {
-            code.Add(0);
+            code += "0";
             hit.Play();
             setText(0);
         }
 		else if (other.gameObject.CompareTag("One"))
         {
-            code.Add(1);
+            code += "1";
             hit.Play();
             setText(1);
         }
 		else if (other.gameObject.CompareTag("Two"))
         {
-            code.Add(2);
+            code += "2";
             hit.Play();
             setText(2);
         }
 		else if (other.gameObject.CompareTag("Three"))
         {
-            code.Add(3);
+            code += "3";
             hit.Play();
             setText(3);
         }
 		else if (other.gameObject.CompareTag("Four"))
         {
-            code.Add(4);
+            code += "4";
             hit.Play();
             setText(4);
         }
 		else if (other.gameObject.CompareTag("Five"))
         {
-            code.Add(5);
+            code += "5";
             hit.Play();
             setText(5);
         }
 		else if (other.gameObject.CompareTag("Six"))
         {
-            code.Add(6);
+            code += "6";
             hit.Play();
             setText(6);
         }
 		else if (other.gameObject.CompareTag("Seven"))
         {
-            code.Add(7);
+            code += "7";
             hit.Play();
             setText(7);
         }
 		else if (other.gameObject.CompareTag("Eight"))
         {
-            code.Add(8);
+            code += "8";
             hit.Play();
             setText(8);
         }
 		else if (other.gameObject.CompareTag("Nine"))
         {
-            code.Add(9);
+            code += "9";
             hit.Play();
             setText(9);
         }
 		else if (other.gameObject.CompareTag("Green"))
 		{
-            if(code.Count == rightCode.Count)
-            {
-                for(int i = 0; i < rightCode.Count; i++)
-                {
-                    if(code[i] != rightCode[i])
-                    {
-                        wrong.Play();
-                        code = new ArrayList();
-                        CodeNum.text = "Code: ";
-                        break;
-                    }
-                    if (i == rightCode.Count - 1)
-                    {
-                        right.Play();
-                        CodeNum.text += "right";
-                        open = true;
-                    }
-                }
-            }
+            if(code == rightCode)
+             {
+                    right.Play();
+                    CodeNum.text += "right";
+                    open = true;
+             }
 			else
 			{
 				wrong.Play();
-				code = new ArrayList();
+				code = "";
                 CodeNum.text = "Code: ";
 			}
 		}
 		else if (other.gameObject.CompareTag("Red"))
 		{
 			wrong.Play();
-			code = new ArrayList();
+			code = "";
             CodeNum.text = "Code: ";
 		}
 
