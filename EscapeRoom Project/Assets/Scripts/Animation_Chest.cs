37348c398ff74481e83ee7bdb3cd3c006511a534
39xdgy <wangjiashu3@126.com>
2018-03-31 18:02:34 -0500

    Make the Animation of chest, have a key on a table

---
+++ EscapeRoom Project/Assets/Scripts/Animation_Chest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Animation_Chest : MonoBehaviour {
+    Player player;
+    Animator anim;
+
+    // Use this for initialization
+    // void Start () {
+
+    //}
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        anim = GetComponent<Animator>();
+        GameObject Hand2 = GameObject.Find("Hand2");
+        //script = Hand2.GetComponent<ControllerButtonPusher>();
+        //playsound.clip = Sound;
+    }
+
+    private void FixedUpdate()
+    {
+        if (player.rightController.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+            anim.SetBool("Open_chest", true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
