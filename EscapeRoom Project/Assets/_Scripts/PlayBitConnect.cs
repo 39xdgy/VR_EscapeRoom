effc241918365ab280773ea711d6b92a55857a88
39xdgy <wangjiashu3@126.com>
2018-04-09 14:47:30 -0500

    really good work

---
+++ EscapeRoom Project/Assets/_Scripts/PlayBitConnect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBitConnect : MonoBehaviour {
+
+    public AudioClip clip;
+
+    void Start()
+    {
+        
+    }
+	// Use this for initialization
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioSource.PlayClipAtPoint(clip, new Vector3(-4.33f, 0.24f, -1.22f));
+    }
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
