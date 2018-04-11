34efaa67821e841c666b97f43f5195e3c1b0fc0e
39xdgy <35781890+39xdgy@users.noreply.github.com>
2018-04-10 20:29:14 -0500

    Documentation

--- EscapeRoom Project/Assets/_Scripts/PlayBitConnect.cs
+++ EscapeRoom Project/Assets/_Scripts/PlayBitConnect.cs
@@ -3,26 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayBitConnect : MonoBehaviour {
-
+	//get the sound clip from the object
     public AudioClip clip;
-
-    void Start()
-    {
-        
-    }
-	// Use this for initialization
-
-
+	// when object collide with this object, play the sound(BitConnect!!!!!!!!!!!!!!!!!!!!!!!!!)
     private void OnTriggerEnter(Collider other)
     {
         AudioSource.PlayClipAtPoint(clip, new Vector3(-4.33f, 0.24f, -1.22f));
     }
-
-
-
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
