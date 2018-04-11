07111f02322bfa56888999d6e8d7d9e4325c96e2
39xdgy <35781890+39xdgy@users.noreply.github.com>
2018-04-10 20:25:54 -0500

    documentation

--- EscapeRoom Project/Assets/_Scripts/ControllerButtonPusher.cs
+++ EscapeRoom Project/Assets/_Scripts/ControllerButtonPusher.cs
@@ -6,42 +6,44 @@ using UnityEngine.UI;
 
 
 public class ControllerButtonPusher : MonoBehaviour {
-
+	// three sound for open the door, wrong code, and hit the button
 	public AudioSource hit;
 	public AudioSource wrong;
 	public AudioSource right;
+	// the boolean is to chack the door is open or not
     public bool open;
+	// text on the top of the keypad
     public Text CodeNum;
+	// code is user input, rightcode is the correct number
     private string code;
     private string rightCode;
 	private Rigidbody rb;
-
+	//calling from a script from another object
     Tutoral_animation script;
 
-
+	//function to add the number after the string of code
     private void setText(int num)
     {
         CodeNum.text += num.ToString() + " ";
     }
 
 
-
+	//setup when the game started
 	void Start ()
 	{
+		//find the door object and pass the script into the script valuable 
         GameObject door = GameObject.Find("Door");
         script = door.GetComponent<Tutoral_animation>();
+		//use the value insdie the other script to this script
         code = script.code;
         rightCode = script.rightcode;
         CodeNum.text = "Password: ";
     }
 
-	void FixedUpdate ()
-	{
-		
-	}
-
+	//when the object collide with the botton
     private void OnTriggerEnter(Collider other)
     {
+		//check which number is pressed, and 1. add the number 2. play the sound 3. add the text on the top of the keypad
         if (other.gameObject.CompareTag("Zero"))
         {
             code += "0";
@@ -102,14 +104,17 @@ public class ControllerButtonPusher : MonoBehaviour {
             hit.Play();
             setText(9);
         }
+		//if the player hit the green key, which they think they got it right
 		else if (other.gameObject.CompareTag("Green"))
 		{
+			//if the code is actually correct, play the right sound, should correct, and open the door
             if(code == rightCode)
              {
                     right.Play();
                     CodeNum.text += "correct";
                     open = true;
              }
+			// this else mean they got it wrong, play the wrong sound and reset the code and the text on the top
 			else
 			{
 				wrong.Play();
@@ -117,16 +122,13 @@ public class ControllerButtonPusher : MonoBehaviour {
                 CodeNum.text = "Password: ";
 			}
 		}
+		//if the player hit the red key, which they want to clean the code
 		else if (other.gameObject.CompareTag("Red"))
 		{
+			//play the sound, reset the code and the text on the top
 			hit.Play();
 			code = "";
             CodeNum.text = "Password: ";
 		}
-
-
-
-
-        //System.Threading.Thread.Sleep(2000);
     }
 }
